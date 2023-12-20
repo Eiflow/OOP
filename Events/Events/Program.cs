59df27682999f807c5bcfe4bd359576139014adc
Ei-Flow <jesper74682gmail.com>
2023-12-20 13:29:41 +0100

    Events

---
+++ Events/Events/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Events
+{
+    internal class Program
+    {
+        static ConsoleColor[] farver = { ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Magenta };
+        static int nuværendeFarveIndex = 0;
+
+        static async Task Main()
+        {
+            Console.WriteLine("Tryk på Enter for at stoppe programmet.");
+
+            do
+            {
+                Console.BackgroundColor = farver[nuværendeFarveIndex];
+                Console.Clear();
+
+                nuværendeFarveIndex = (nuværendeFarveIndex + 1) % farver.Length;
+
+                await Task.Delay(5000);
+
+            } while (!Console.KeyAvailable || Console.ReadKey(true).Key != ConsoleKey.Enter);
+        }
+    }
+}
